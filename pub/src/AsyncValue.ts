0418633a2085a37d4bcb367d77f7d2e6012d0bb0
corno <corno.astn.a@schraverus.com>
2022-09-06 12:54:20 +0200

    .

--- pub/src/AsyncValue.ts
+++ pub/src/AsyncValue.ts
@@ -1,6 +1,13 @@
 
 export type AsyncValue<T> = {
-    execute: (
+    map: <NT>($c: ($: T) => AsyncValue<NT>) => AsyncValue<NT>
+
+    /**
+     * if the callback returns undefined, the resulting asynchronous value is never evaluated
+     * the async chain stops.
+     */
+    setCondition: <NT>($c: ($: T) => AsyncValue<NT> | undefined) => AsyncValue<NT>
+    _execute: (
         callback: ($: T) => void
     ) => void
 }
