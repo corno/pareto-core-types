3e481df6fec6c2cd109001562522e2da074f4fcc
Corno Schraverus <corno@schraverus.com>
2024-08-18 03:25:48 +0200

    more comments

--- pub/src/Nested.ts
+++ pub/src/Nested.ts
@@ -2,5 +2,9 @@
 /**
  * this type allows to nest a set of data types
  * example: const A: Nested<number> = [ 1, [ 2, 3 ] ]
+ * 
+ * caution: the type T cannot be an array, because then it is not clear what an empty array would mean;
+ * -a T value
+ * -or a Nested<T>[]
  */
 export type Nested<T> = T | Nested<T>[]
--- pub/src/OptionalValue.ts
+++ pub/src/OptionalValue.ts
@@ -6,3 +6,19 @@
  * this 'OptionalValue' type makes it possible to have recursive optional types like this: OptionalValue<OptionalValue<number>>
  */
 export type OptionalValue<T> = [false] | [true, T]
+
+/*
+TBD: why is this not an interface with a map function?
+
+one reason I can think of is that it allows for a switch like this:
+
+switch (x[0]) {
+    case false:
+        //do something
+    case true:
+        ss($[i], ($) => {
+            //do something else
+        })
+    default: au(x[0])
+}
+*/
