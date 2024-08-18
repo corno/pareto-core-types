ee0a7176b417ec8500b4cd67e60a8c0bef106bb9
Corno Schraverus <corno@schraverus.com>
2024-08-18 10:16:56 +0200

    made OptionalValue an interface

--- pub/src/OptionalValue.ts
+++ pub/src/OptionalValue.ts
@@ -1,24 +1,26 @@
 
 /**
+ * this is literal data type, either [false] or [true, T]
+ * the first entry of the tuple is thus 'false' or 'true'. If the first one is 'true',
+ * then there is a second one containing the value T
+ */
+export type RawOptionalValue<T> = [false] | [true, T]
+
+/**
  * Why this type and not use for example 'null | T'?
  * the 'null | T' is vulnerable. If you have a parametrized function 'foo<T>() null | T' and T is null | number,
  * you cannot discern if a return value is null because of the function or because of the data
  * this 'OptionalValue' type makes it possible to have recursive optional types like this: OptionalValue<OptionalValue<number>>
  */
-export type OptionalValue<T> = [false] | [true, T]
+export interface OptionalValue<T> {
+    /**
+     * 
+     * @param $v callback to transform an individual entry. keys are not available.
+     */
+    map<NT>(
+        set: ($: T) => NT,
+        notSet: () => NT,
+    ): OptionalValue<NT>
 
-/*
-TBD: why is this not an interface with a map function?
-
-one reason I can think of is that it allows for a switch like this:
-
-switch (x[0]) {
-    case false:
-        //do something
-    case true:
-        ss($[i], ($) => {
-            //do something else
-        })
-    default: au(x[0])
+    raw: RawOptionalValue<T>
 }
-*/
